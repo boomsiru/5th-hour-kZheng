20da718e4aae642fb66136da174c65ec686677ee
Kevin Zheng <kz126156@student.wpsok.org>
2024-08-21 12:56:40 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,16 @@
+#Name Kevin Zheng=
+#Class 5th hour
+#Assignment HW2
+
+print("hello world")
+
+print("What is your name")
+input("")
+
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)\
+    #the GOAT https://www.w3schools.com/python/python_howto_add_two_numbers.asp
