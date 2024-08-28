853047ab28bbc9f60ed7b5513ad8a61a29b16bce
Kevin Zheng <kz126156@student.wpsok.org>
2024-08-28 13:01:42 -0500

    added HW3

--- HW2.py
+++ HW2.py
@@ -1,16 +1,16 @@
 #Name Kevin Zheng=
 #Class 5th hour
 #Assignment HW2
-
+# the GOAT https://www.w3schools.com/python/python_howto_add_two_numbers.asp
 print("hello world")
 
 print("What is your name")
 input("")
 
-x = input("Type a number: ")
-y = input("Type another number: ")
+x = int(input("Type a number: "))
+y = int(input("Type another number: "))
 
-sum = int(x) + int(y)
+sum = (x) + (y)
 
-print("The sum is: ", sum)\
-    #the GOAT https://www.w3schools.com/python/python_howto_add_two_numbers.asp
+print("The sum is: ", sum)
+
---
+++ HW3.py
@@ -0,0 +1,15 @@
+#Name Kevin
+#Class 5th
+#assignment HW3
+
+print("hello world")
+monkeList = ["Monke1" , "Monke2" , "Monke3" , "Monke4" , "Monke5"]
+monkeList.append(input("please enter monke"))
+print(f"{monkeList[3]}")
+
+NumList = [ 1, 4, 3, 4,]
+NumList.insert(1, 1)
+print(NumList)
+NumList.sort()
+sum = NumList[0] + NumList[1] + NumList[2]
+print(sum)
