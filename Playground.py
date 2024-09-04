820656db1891c0c82a5c09e482cee666ed9dc94f
Kevin Zheng <kz126156@student.wpsok.org>
2024-09-04 13:19:31 -0500

    added playground

---
+++ Playground.py
@@ -0,0 +1,12 @@
+#Name Kevin Zheng
+#class 5th hour
+#assignment playground
+
+
+x = int(input("Type a number: "))
+y = int(input("Type another number: "))
+
+sum = (x) + (y)
+
+print("The sum is: ", sum)
+
