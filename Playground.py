936c328f9558f68646942abb835419d7551a7f3a
Kevin Zheng <kz126156@student.wpsok.org>
2024-09-05 13:00:45 -0500

    made changes to playground

--- Playground.py
+++ Playground.py
@@ -1,12 +1,25 @@
 #Name Kevin Zheng
 #class 5th hour
 #assignment playground
+print("Hello, please enter a name")
+input()
+print("Trick question u are a Monke")
 
-
-x = int(input("Type a number: "))
+x = int(input("Now type a number: "))
+if x > 0:
+    print("You are a positive monke")
+elif x < 0:
+    print("You are a negative monke")
+else:
+    print("You stupid monke")
 y = int(input("Type another number: "))
-
+if y > 0:
+    print("You are a positive monke again")
+elif y < 0:
+    print("You are a negative monke again")
+else:
+    print("You are a goober")
 sum = (x) + (y)
 
-print("The sum is: ", sum)
+print("The sum is:", sum, "stupid monke")
 
