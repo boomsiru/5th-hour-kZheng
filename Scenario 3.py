1399d38022a78a44d80524a2b502f3eb5674253b
Kevin Zheng <kz126156@student.wpsok.org>
2024-10-15 13:17:42 -0500

    added changes to scenario 3

--- Scenario 3.py
+++ Scenario 3.py
@@ -58,19 +58,20 @@ def rolld20():
     return random.randint(1,20)
 
 class Character:
-    def __init__(self, name, attack_power,health, AC ):
+    def __init__(self, name, attack_power,health, AC, hit ):
         self.name = name
         self.attack_power = attack_power
         self.health = health
         self.AC = AC
+        self.hit = hit
     def attack(self, target):
-        attkdmg = self.attack_power - target.AC
+        if target.AC > self.hit:
+            self.attack_power = 0
 
-        if attkdmg <= 0:
-            attkdmg = 0
-        print(f"{self.name} attacks {target.name} for {attkdmg} damage!")
 
-        target.health -= attkdmg
+        print(f"{self.name} attacks {target.name} for {self.attack_power} damage!")
+
+        target.health -= self.attack_power
 
         if self.health <= 0:
             print( self.name, "is dead")
@@ -85,11 +86,11 @@ class Character:
 
 
 
-Astarion = Character("Astarion", rolld6()+rolld6(), 10, 14)
-Shadowheart = Character("Shadowheart", rolld10()+2, 10, 14)
-Gale = Character("Gale", rolld6()+rolld6(), 8,  14)
-Laezel = Character("Laezel", rolld6()+2, 12, 17)
-CuriousGeorge = Character("Curious George", rolld20() + 100, 100, 20)
+Astarion = Character("Astarion", rolld6()+rolld6(), 10, 14, rolld20()+4)
+Shadowheart = Character("Shadowheart", rolld10()+2, 10, 14, rolld20()+3)
+Gale = Character("Gale", rolld6()+rolld6(), 8,  14, rolld20()+2)
+Laezel = Character("Laezel", rolld6()+ 20, 12, 17, rolld20()+20)
+CuriousGeorge = Character("Curious George", rolld20() + 100, 100, 20, rolld20()+100)
 
 def perform_attacks(characters, target):
     for character in characters:
