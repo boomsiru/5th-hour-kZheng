4c74a783276a0bef902fdf59882f14b0541a38d0
Kevin Zheng <kz126156@student.wpsok.org>
2024-10-23 13:17:05 -0500

    added changes to HW8 and added HW9

---
+++ HW9.py
@@ -0,0 +1,20 @@
+#Name:Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+print("Hello Monkeys")
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+#if temperature is >20 print it's hot and thank you then if temperature is >10 print it's mild and thank you
+#if not print it's cold and thank you
+
+temperature = 20
+if temperature > 20:
+    print("It's hot ")
+elif temperature >= 10:
+    print("it's mild ")
+else:
+    print("It's cold")
+print('"Thank You Monke," said Frieza. ')
