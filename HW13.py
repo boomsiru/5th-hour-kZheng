5bd6f8f42f63aac057593fc13fef555ba89e7181
Kevin Zheng <kz126156@student.wpsok.org>
2024-11-13 12:58:42 -0600

    added HW13 and HW14

---
+++ HW13.py
@@ -0,0 +1,25 @@
+#Name:Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW13
+
+
+#1. Create a list containing 10 integers of your choice.
+NumberList = [1,2,3,4,5,6,7,8,9,10]
+#2. Create two empty variables named evenNumbers and oddNumbers.
+evenNumbers = ""
+oddNumbers = ""
+#3. Make a loop that counts the number of even and odd numbers in the list.
+#(HINT: The way to see if a number is even is if it is divisible by 2).
+k = 0
+for NumberList in range(0, 10):
+    if NumberList % 2 ==0:
+        k +=1
+    else:
+        k +=1
+print(k)
+
+
+
+
+
+# Print the total count of even and odd numbers.
---
+++ HW14.py
@@ -0,0 +1,24 @@
+#Name:Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW14
+
+import math
+from math import factorial
+
+#1. Create a variable that asks the user for an integer and an empty intger variable.
+monk = int(input("Give me numbe monke"))
+
+
+factorial = 1
+for i in range(1, monk + 1):
+    factorial *= i
+print(factorial)
+#2. Create a loop with a range from 1 to the number the user input.
+
+
+
+#3. Use the loop to find the factorial of that number. A factorial of a number is that number multiplied
+#by every number before it until you reach 1.
+#For example: 5! is 5 x 4 x 3 x 2 x 1 = 120
+
+#4. Print the factorial.
