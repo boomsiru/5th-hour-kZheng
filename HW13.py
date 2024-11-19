b2f59ca9e44b7d2a92425141e06418b9b5b25600
Kevin Zheng <kz126156@student.wpsok.org>
2024-11-19 12:56:06 -0600

    added changes to HW13

--- HW13.py
+++ HW13.py
@@ -6,17 +6,19 @@
 #1. Create a list containing 10 integers of your choice.
 NumberList = [1,2,3,4,5,6,7,8,9,10]
 #2. Create two empty variables named evenNumbers and oddNumbers.
-evenNumbers = ""
-oddNumbers = ""
+evenNumbers = 0
+oddNumbers = 0
 #3. Make a loop that counts the number of even and odd numbers in the list.
 #(HINT: The way to see if a number is even is if it is divisible by 2).
-k = 0
+
 for NumberList in range(0, 10):
     if NumberList % 2 ==0:
-        k +=1
+        evenNumbers +=1
     else:
-        k +=1
-print(k)
+        oddNumbers +=1
+print(oddNumbers)
+print(evenNumbers)
+print(oddNumbers + evenNumbers)
 
 
 
