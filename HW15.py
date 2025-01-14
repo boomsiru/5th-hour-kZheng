69a899e62b7506ac1c8ee1ec593ccf9f06fa6e78
Kevin Zheng <kz126156@student.wpsok.org>
2025-01-14 13:16:37 -0600

    added HW 15 and 16

---
+++ HW15.py
@@ -0,0 +1,33 @@
+#Name:Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW15
+import random
+#1. Create a def function that prints out "Hello World!"
+def monke():
+    print("hello monke")
+#2. Create a def function that calculates the average of three numbers.
+def AvgmMonke():
+    a= random.randint(1,3)
+    b = random.randint(1, 3)
+    c = random.randint(1, 3)
+    avg = a+b+c / 3
+    print(avg)
+
+#3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
+#prints the name of the third animal.
+def Monke_list(*monkey):
+    print("The 3rd monke is", monkey[2])
+Monke_list("Ape", "Chimp", "monke", "Kong", "fraud")
+
+
+#4. Create a def function that loops from 1 to the number put in the argument.
+def monkeloop():
+    t = int(input("please enter number"))
+    for x in range(1,t+1):
+        print(x)
+
+
+#5. Call all of the functions created in 1 - 4 with relevant arguments.
+monkeloop()
+monke()
+AvgmMonke()
---
+++ HW16.py
@@ -0,0 +1,80 @@
+#Name:Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW16
+import random
+#1. Create a def function that plays a single round of rock, paper, scissors where the user inputs
+#1 for rock, 2 for paper, or 3 for scissors and compares it to a random number generated to serve
+#as the "opponent's hand".
+def Jaken():
+    while True:
+
+        print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+
+        # Take the input from user
+        choice = int(input("Enter your choice: "))
+
+        # Looping until user enters valid input
+        while choice > 3 or choice < 1:
+            choice = int(input('Enter a valid choice please ☺: '))
+
+        # Initialize value of choice_name variable corresponding to the choice value
+        if choice == 1:
+            choice_name = 'Rock'
+        elif choice == 2:
+            choice_name = 'Paper'
+        else:
+            choice_name = 'Scissors'
+
+        # Print user choice
+        print('User choice is:', choice_name)
+        print("Now it's Computer's Turn...")
+
+        # Computer chooses randomly any number among 1, 2, and 3
+        comp_choice = random.randint(1, 3)
+
+        # Initialize value of comp_choice_name variable corresponding to the choice value
+        if comp_choice == 1:
+            comp_choice_name = 'Rock'
+        elif comp_choice == 2:
+            comp_choice_name = 'Paper'
+        else:
+            comp_choice_name = 'Scissors'
+
+        print("Computer choice is:", comp_choice_name)
+        print(choice_name, 'vs', comp_choice_name)
+
+        # Determine the winner
+        if choice == comp_choice:
+            result = "DRAW"
+        elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
+            result = 'Paper'
+        elif (choice == 1 and comp_choice == 3) or (comp_choice == 1 and choice == 3):
+            result = 'Rock'
+        elif (choice == 2 and comp_choice == 3) or (comp_choice == 2 and choice == 3):
+            result = 'Scissors'
+
+        # Print the result
+        if result == "DRAW":
+            print("<== It's a tie! ==>")
+        elif result == choice_name:
+            print("<== User wins! ==>")
+        else:
+            print("<== Computer wins! ==>")
+
+        # Ask if the user wants to play again
+        loop()
+
+    # After coming out of the while loop, print thanks for playing
+
+
+#2. Create a def function that prompts the user to input if they want to play another round, and
+#repeats the RPS def function if they do or exits the code if they don't.
+def loop():
+    print("Do you want to play again? (Y/N)")
+    ans = input().lower()
+    if ans == 'n':
+        print("Thanks for playing!")
+        exit()
+
+Jaken()
+
