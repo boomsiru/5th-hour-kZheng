bde50e863287a139021de79422bf443c12fc3dcf
Kevin Zheng <kz126156@student.wpsok.org>
2025-01-31 12:41:41 -0600

    added HW19

---
+++ HW19.py
@@ -0,0 +1,10 @@
+
+#Name: Kevin Zheng
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from HW15 import AvgmMonke, monke, Monke_list, monkeloop, random
+#2. Call the functions here and run HW19.
+
+#3. Delete all calls from HW15 and run HW19 again.
