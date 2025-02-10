51a35e88b601df87b1a36c292945ddac8e24d876
Kevin Zheng <kz126156@student.wpsok.org>
2025-02-10 12:43:41 -0600

    added changes to HW19

--- HW19.py
+++ HW19.py
@@ -6,5 +6,8 @@
 #1. Import all the functions from the HW15 file.
 from HW15 import AvgmMonke, monke, Monke_list, monkeloop, random
 #2. Call the functions here and run HW19.
-
+monke()
+AvgmMonke(a = random.randint(1,3), b = random.randint(1, 3), c = random.randint(1, 3) )
+Monke_list("Ape", "Chimp", "monke", "Kong", "fraud")
+monkeloop(t = int(input("please enter number")))
 #3. Delete all calls from HW15 and run HW19 again.
